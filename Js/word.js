c91f83e4aa80a72c76c3020645923c1d296c620d
sandychen014035 <tp6u6g0@gmail.com>
2024-09-20 23:05:27 +0800

    0920

--- Js/word.js
+++ Js/word.js
@@ -1,18 +1,18 @@
-document.querySelectorAll('.secondaryMenu a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); // 防止連結跳轉
-        
+document.querySelectorAll('.secondaryMenu a').forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault(); 
+
         // 取得點擊的 data-class 屬性值
         var selectedClass = link.getAttribute('data-class');
-        
-        // 隱藏所有 .A 區域中的 div
-        document.querySelectorAll('.A div').forEach(function(div) {
-            div.style.display = 'none';
+
+        // 隱藏所有 .worksField 內的元素
+        document.querySelectorAll('.worksField').forEach(function (field) {
+            field.style.display = 'none';
         });
-        
+
         // 顯示選擇的 class 元素
-        document.querySelectorAll('.A .' + selectedClass).forEach(function(div) {
-            div.style.display = 'block';
+        document.querySelectorAll('.' + selectedClass).forEach(function (div) {
+            div.parentElement.style.display = 'flex';
         });
     });
 });
